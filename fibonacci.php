f816c99950d42896957987fb950a3595dc68844d
Kirill Chertkov <chertkov.k@gmail.com>
2018-04-24 01:58:15 +0300

    add fibonacci.php

---
+++ fibonacci.php
@@ -0,0 +1,15 @@
+function fib($num)
+{
+    if ($num === 0 || $num === 1) {
+        return $num;
+    }
+    $first = 0;
+    $next = 1;
+    $fib = 0;
+    for ($i = $num; $i > 1; $i--) {
+        $fib = $first + $next;
+        $first = $next;
+        $next = $fib;
+    }
+    return $fib;
+}
